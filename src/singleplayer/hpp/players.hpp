134e65694900023df32823e6d757b1be56166409
Vladz <hramzik.ru@yandex.ru>
2023-08-19 17:51:29 +0300

    making sdl_texture a part of the player

---
+++ src/singleplayer/hpp/players.hpp
@@ -0,0 +1,10 @@
+#ifndef SINGLEPLAYER_PLAYERS_HPP_INCLUDED
+#define SINGLEPLAYER_PLAYERS_HPP_INCLUDED
+//--------------------------------------------------
+
+
+DEF_SKIN ("media/doodler1.png", )
+
+
+//--------------------------------------------------
+#endif
