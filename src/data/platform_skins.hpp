d3efde5cbd19cd95be0b12471bf1830ae61d1289
Vladz <hramzik.ru@yandex.ru>
2023-09-19 00:09:34 +0300

    pushing to remote repository..

--- src/data/platform_skins.hpp
+++ src/data/platform_skins.hpp
@@ -3,22 +3,28 @@
 
 DEF_SKIN (0, "platform.png",
     DEF_TEXTURE_OFFSET (-57, 30, 115, 30),
-    DEF_PLATFORM_TYPE (PT_DEFAULT)
-    DEF_PLATFORM_TYPE (PT_MOVING),
+    DEF_PLATFORM_TYPE (PT_DEFAULT),
     DEF_HITBOX_RECT (-55, 28, 110, 28)
 )
 
 
-DEF_SKIN (0, "platform_fake.png",
+DEF_SKIN (0, "fake.png",
     DEF_TEXTURE_OFFSET (-57, 30, 115, 30),
     DEF_PLATFORM_TYPE (PT_FAKE),
     DEF_HITBOX_RECT (-55, 28, 110, 28)
 )
 
 
-DEF_SKIN (0, "platform_cloud.png",
+DEF_SKIN (0, "cloud.png",
     DEF_TEXTURE_OFFSET (-57, 30, 115, 30),
     DEF_PLATFORM_TYPE (PT_CLOUD),
+    DEF_HITBOX_RECT (-55, 28, 110, 28)
+)
+
+
+DEF_SKIN (0, "moving.png",
+    DEF_TEXTURE_OFFSET (-57, 30, 115, 30),
+    DEF_PLATFORM_TYPE (PT_MOVING),
     DEF_HITBOX_RECT (-55, 28, 110, 28)
 )
 
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 int main (void) {
 
     srand ( (unsigned) time (nullptr));
+    initialize_sdl ();
 
     Game* game = create_game ();
 
@@ -22,7 +23,7 @@ int main (void) {
 
     printf ("goodbye!\n");
 
-
+    quit_sdl ();
     return 0;
 }
 
